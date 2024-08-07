b7cfe0856feb3d96dae97144be9bc8800a7276ee
val <val@VALENEITOR10>
2024-08-07 01:26:12 -0300

    Add project files.

---
+++ net-employee-CRUD/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace net_employee_CRUD.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public int? Salary { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+}
---
+++ net-employee-CRUD/Models/EmployeeDataContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace net_employee_CRUD.Models;
+
+public partial class EmployeeDataContext : DbContext
+{
+    public EmployeeDataContext()
+    {
+    }
+
+    public EmployeeDataContext(DbContextOptions<EmployeeDataContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=employee_data;Username=postgres;Password=secret");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("employee_pkey");
+
+            entity.ToTable("employee");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .HasColumnName("email");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .HasColumnName("name");
+            entity.Property(e => e.Salary).HasColumnName("salary");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
